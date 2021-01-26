c9a7167b04668d7ef89391cfd0f6def8b277927a
Sugam Devare <sugamxp@gmail.com>
2021-01-26 19:52:30 +0530

    button click test done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently</h1>
-      <button data-test="increment-button">Increment Counter</button>
+      <h1 data-test="counter-display">
+        The counter is currently <span data-test="count">{count}</span>{" "}
+      </h1>
+      <button
+        data-test="increment-button"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Increment Counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -33,6 +33,16 @@ test("renders counter display", () => {
   expect(counterDisplay.length).toBe(1);
 });
 
-test("counter starts at 0", () => {});
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+});
 
-test("clicking on button increments counter display", () => {});
+test("clicking on button increments counter display", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate("click");
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+});
