c27e946a28748f78f8021b961cf40c8ecae4a739
Sugam Devare <sugamxp@gmail.com>
2021-01-26 19:29:14 +0530

    data-test attribute added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div data-test="component-app">
-      <h1>Counter</h1>
+      <h1 data-test="counter-display">The counter is currently</h1>
+      <button data-test="increment-button">Increment Counter</button>
     </div>
   );
 }
