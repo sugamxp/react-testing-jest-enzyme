f42d5dff06230d81fb24eb2ef18f5fe59ac83bbe
Sugam Devare <sugamxp@gmail.com>
2021-01-26 19:37:20 +0530

    test refactored - dry

--- src/App.test.js
+++ src/App.test.js
@@ -6,21 +6,30 @@ import App from "./App";
 //set up enzyme' react adapter
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ * Factory fuction to create a ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 test("renders button", () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
 
